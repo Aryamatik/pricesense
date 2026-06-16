8af9174bedc3b7fca43d9b93d89856953e4db965
Ansh Kyal <159647320+Anshkyal@users.noreply.github.com>
2026-06-16 23:11:28 +0530

    phase 3 (recommendations done)
    (recommendations done)

--- sql/05_Extra.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    COUNT(*) AS total_rows,
-    COUNT(*) FILTER (WHERE price > 1000) AS rows_above_1000,
-    ROUND(
-        100.0 * COUNT(*) FILTER (WHERE price > 1000) / COUNT(*),
-        2
-    ) AS pct_above_1000
-FROM master;
